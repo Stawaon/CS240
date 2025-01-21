d22fef9aac5c194b8c914f468407bf038e282790
Jasia Ernest <119990861+JasiaErnest@users.noreply.github.com>
2025-01-21 13:29:15 -0800

    Update towersOfHanoi.py

--- Lab 1/towersOfHanoi.py
+++ Lab 1/towersOfHanoi.py
@@ -1,3 +1,7 @@
+#CS240 Lab 1 - Towers of Hanoi
+#This is a towers of Hanoi program that takes the number of disks as input and prints the moves and number of moves to solve the puzzle. 
+
+#Helper function to move the disks
 def move(n, src, dest, count):
     lastIdx = len(src)-1
     val = src[lastIdx]
@@ -5,7 +9,8 @@ def move(n, src, dest, count):
     dest.append(val)
     src.pop(lastIdx)
     count.append(1)
-    
+
+#Main recursive function to solve the puzzle    
 def stack(n, src, aux, dest, count):
     if (n == 0):
         return
@@ -13,14 +18,17 @@ def stack(n, src, aux, dest, count):
     move(n, src, dest, count)
     stack(n - 1, aux, src, dest, count)
     return count
-        
+
 diskNum = int(input("Enter the number of disks: "))
 src = []
 aux = []
 dest = []
 count = []
+#Creates the source stack
 for i in range(diskNum):
     src.append(diskNum - i)
 print("The moves are:")
+#Calls the stack function to solve the puzzle
 stack(len(src), src, aux, dest, count)
+#Prints the total number of moves
 print("Total number of moves:", len(count))
